c9570976e27f933595b95eeefe79f6662d30005f
Guilherme-FCM <guilherme_fcm@hotmail.com>
2023-03-05 11:57:41 -0300

    Show data on PlanetActivity

--- app/src/main/java/com/example/planets/PlanetActivity.java
+++ app/src/main/java/com/example/planets/PlanetActivity.java
@@ -4,19 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.Serializable;
 
 public class PlanetActivity extends AppCompatActivity {
+    private ImageView planetImageView;
+    private TextView planetNameView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_planet);
 
+        planetImageView = findViewById(R.id.planetImage);
+        planetNameView = findViewById(R.id.planetName);
+
         Intent intent = getIntent();
         Planet planet = (Planet) intent.getSerializableExtra("planet");
-        Toast.makeText(PlanetActivity.this, planet.toString(), Toast.LENGTH_SHORT).show();
+
+        planetImageView.setImageResource(planet.getImage());
+        planetNameView.setText(planet.getName());
     }
 }
